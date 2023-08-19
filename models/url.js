e82c8f7de509939ef97fe880e8d6df17cb914585
Anil <kanilreddy867@gmail.com>
2023-08-19 21:57:56 +0530

    initial stage has been completed

---
+++ models/url.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const shortUrlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortCode: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  clickCount: {
+    type: Number,
+    default: 0,
+  },
+  customAlias: {
+    type: String,
+    unique: true,
+  },
+  metadata: {
+    userAgent: String,
+    ipAddress: String,
+    referralSource: String,
+  },
+});
+
+const ShortUrl = mongoose.model("ShortUrl", shortUrlSchema);
+
+module.exports = { ShortUrl };
