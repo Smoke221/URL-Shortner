82c666d2fdbb8610bcac06c90ab43e50d4d7ff63
Anil <kanilreddy867@gmail.com>
2023-08-20 23:42:19 +0530

    testing nano id package

--- routes/url.js
+++ routes/url.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const nanoid = require("nano-id")
 const { ShortUrlModel } = require("express");
 
 const urlRouter = express.Router();
 
+console.log(2+4)
 module.exports = { urlRouter };
